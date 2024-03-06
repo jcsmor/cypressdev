f41cbda0927db7e7ffe99b8eece05d94e2f80260
jcsmor <jdaimon@gmail.com>
2024-03-06 18:12:44 +0000

    adding

--- cypress/integration/examples/injectJWT.js
+++ cypress/integration/examples/injectJWT.js
@@ -2,55 +2,39 @@
 
 //const neatCSV = require('neat-csv')
 
-import neatCSV from 'neat-csv';
+//import neatCSV from 'neat-csv';
 
 let productName
 
 describe('JWT Session', () => {
 
   it('is logged in through local storage', async () => {
-
-
-
     cy.LoginAPI().then(function () {
-
       cy.visit("https://rahulshettyacademy.com/client",
-
         {
-
           onBeforeLoad: function (window) {
-
             window.localStorage.setItem('token', Cypress.env('token'))
-
           }
-
-
-
         })
-
-
-
     })
 
     cy.get(".card-body b").eq(1).then(function (ele) {
-
       productName = ele.text();
-
     })
 
-    cy.get(".card-body button:last-of-type").eq(1).click();
+    cy.get(".card-body button:last-of-type").eq(0).click();
 
     cy.get("[routerlink*='cart']").click();
 
     cy.contains("Checkout").click();
 
-    cy.get("[placeholder*='Country']").type("ind")
+    cy.get("[placeholder*='Country']").type("port")
 
     cy.get('.ta-results button').each(($e1, index, $list) => {
 
 
 
-      if ($e1.text() === " India") {
+      if ($e1.text() === " Portugal") {
 
         cy.wrap($e1).click()
 
@@ -66,19 +50,19 @@ describe('JWT Session', () => {
 
 
 
-    cy.readFile(Cypress.config("fileServerFolder") + "/cypress/downloads/order-invoice_rahul.csv")
+    // cy.readFile(Cypress.config("fileServerFolder") + "/cypress/downloads/order-invoice_rahul.csv")
 
-      .then(async (text) => {
+    //   .then(async (text) => {
 
-        const csv = await neatCSV(text)
+    //     const csv = await neatCSV(text)
 
-        console.log(csv)
+    //     console.log(csv)
 
-        const actualProductCSV = csv[0]["Product Name"]
+    //     const actualProductCSV = csv[0]["Product Name"]
 
-        expect(productName).to.equal(actualProductCSV)
+    //     expect(productName).to.equal(actualProductCSV)
 
-      })
+    //   })
 
   })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -13,28 +13,27 @@
 // Cypress.Commands.add("login", (email, password) => { ... })
 //
 
-Cypress.Commands.add("selectProduct", (productName) => { 
+Cypress.Commands.add("selectProduct", (productName) => {
     cy.get('h4.card-title').each(($el, index, $list) => {
-        if($el.text().includes(productName))
-        {
+        if ($el.text().includes(productName)) {
             cy.get('button.btn.btn-info').eq(index).click()
         }
-        
-        })
 
-
-})
-
-Cypress.Commands.add("LoginAPI",()=> {
-
-    cy.request("POST","https://rahulshettyacademy.com/api/ecom/auth/login",
-    {"userEmail":"anshika@gmail.com","userPassword":"Iamking@000"}).
-    then(function(response)
-    {
-       expect(response.status).to.eq(200)
-       Cypress.env('token',response.body.token);
     })
+
+
 })
+
+Cypress.Commands.add("LoginAPI", () => {
+
+    cy.request("POST", "https://rahulshettyacademy.com/api/ecom/auth/login",
+        { "userEmail": "anshika@gmail.com", "userPassword": "Iamking@000" }).
+        then(function (response) {
+            expect(response.status).to.eq(200)
+            Cypress.env('token', response.body.token);
+        })
+})
+
 
 
 
