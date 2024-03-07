7d8e467df6677f520693788f379d2c45ee08f961
jcsmor <jdaimon@gmail.com>
2024-03-07 16:59:35 +0000

    ss

--- cypress/integration/examples/sqldb.js
+++ cypress/integration/examples/sqldb.js
@@ -5,7 +5,7 @@ describe('My Second Test Suite', function () {
     it('My FirstTest case', function () {
         cy.log("hello")
         cy.sqlServer("select * from Persons").then(function (result) {
-            console.log(result[0][1])
+            cy.log(result[0][1])
         })
     })
 
