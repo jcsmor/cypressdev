16dec3e3252943f702ea274b1ced9b45502d22b6
jcsmor <jdaimon@gmail.com>
2024-03-07 16:47:46 +0000

    sql db

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,26 @@
 const { defineConfig } = require("cypress");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+const sqlServer = require('cypress-sql-server');
 
 async function setupNodeEvents(on, config) {
   // loads all plugins before runs the tests
   // implement node event listners here
+
+  config.db = {
+    userName: "dadm",
+    password: "Daimonlux137",
+    server: "daimonserver.database.windows.net",
+    options: {
+      database: "daimondb",
+      encrypt: true,
+      rowCollectionOnRequestCompletion: true
+    }
+  }
+
   await preprocessor.addCucumberPreprocessorPlugin(on, config)
+  tasks = sqlServer.loadDBPlugin(config.db);
+  on('task', tasks);
   on("file:preprocessor", browserify.default(config));
   // Make sure to return the config object as it might have been modified by the plugin
   return config;
--- cypress/integration/examples/sqldb.js
+++ cypress/integration/examples/sqldb.js
@@ -1,17 +1,12 @@
-/*
 /// <reference types="Cypress" />
 
-describe('My Second Test Suite', function() 
-{
+describe('My Second Test Suite', function () {
 
-it('My FirstTest case',function() {
-    cy.log("hello")
-    cy.sqlServer("select * from Person").then(function(result)
-    {
-        console.log(result[0][1])
+    it('My FirstTest case', function () {
+        cy.log("hello")
+        cy.sqlServer("select * from Persons").then(function (result) {
+            console.log(result[0][1])
+        })
     })
-})
 
 })
-
-*/
