dbe72b3af0fefa3fad354a3dda0f5e30a86710c5
jcsmor <jdaimon@gmail.com>
2024-03-13 01:36:46 +0000

    vvv

--- cypress/integration/examples/test.js
+++ cypress/integration/examples/test.js
@@ -1,21 +1,17 @@
-console.log("Hello world!")
-
-var scores = [12,13,14,16]
-var evenScores=[]
-for(let i=0;i<scores.length;i++)
-{
-    if(scores[i] % 2 ==  0)
-    {
+var scores = [12, 13, 14, 16]
+var evenScores = []
+for (let i = 0; i < scores.length; i++) {
+    if (scores[i] % 2 == 0) {
         evenScores.push(scores[i])
     }
 }
 
 console.log(evenScores)
 
-let scores1 = [12,13,14,16]
+let scores1 = [12, 13, 14, 16]
 
-let sumValue = scores1.filter(score=>score%2==0)
-.map(score=>score*3)
-.reduce((sum, val)=>sum+val, 0)
+let sumValue = scores1.filter(score => score % 2 == 0)
+    .map(score => score * 3)
+    .reduce((sum, val) => sum + val, 0)
 
 console.log(sumValue)
