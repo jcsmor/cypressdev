4cbd42911e5b64c0e2fa0bc25d47ecba7c83d713
jcsmor <jdaimon@gmail.com>
2024-03-14 13:44:55 +0000

    ss

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -6,8 +6,8 @@ describe('My First Test Suite', () => {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
     cy.get('.search-keyword').type('ca')
     cy.wait(2000)
-    //cy.get('.products .product').should('have.length', 4)
-    // console.log() -> prints on dev tools log
+    //cy.get('.products .product').should('have.length', 4)  //tagName[attribute = value]
+    // console.log() -> prints on dev tools log              // input[type='search']
     // we can use jquery to get only the visible products
     // jquery--> cy.get('.product:visible').should('have.length', 4)
     cy.get('.products').as('productLocator')
